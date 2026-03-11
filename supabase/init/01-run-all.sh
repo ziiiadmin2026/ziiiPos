4e2ef242db535e848df35367c42bd0799ce45264
ziiiadmin2026 <johnattanm3007@outlook.com>
2026-03-10 21:49:35 -0600

    Align internal Supabase service roles

--- supabase/init/01-run-all.sh
+++ supabase/init/01-run-all.sh
@@ -4,6 +4,35 @@ set -eu
 DB_ADMIN_USER="${SUPABASE_DB_ADMIN_USER:-supabase_admin}"
 BOOTSTRAP_MARKER="/var/lib/postgresql/data/.pos-bootstrap-complete"
 
+psql -v ON_ERROR_STOP=1 -U "$DB_ADMIN_USER" -d postgres <<SQL
+do \
+\$\$
+begin
+  if not exists (select 1 from pg_roles where rolname = 'supabase_auth_admin') then
+    create role supabase_auth_admin login password '${POSTGRES_PASSWORD}';
+  else
+    alter role supabase_auth_admin with login password '${POSTGRES_PASSWORD}';
+  end if;
+
+  if not exists (select 1 from pg_roles where rolname = 'supabase_storage_admin') then
+    create role supabase_storage_admin login password '${POSTGRES_PASSWORD}';
+  else
+    alter role supabase_storage_admin with login password '${POSTGRES_PASSWORD}';
+  end if;
+
+  if not exists (select 1 from pg_roles where rolname = 'authenticator') then
+    create role authenticator login password '${POSTGRES_PASSWORD}' noinherit;
+  else
+    alter role authenticator with login password '${POSTGRES_PASSWORD}' noinherit;
+  end if;
+
+  grant anon to authenticator;
+  grant authenticated to authenticator;
+  grant service_role to authenticator;
+end;
+\$\$;
+SQL
+
 for file in /pos-bootstrap/migrations/*.sql; do
   if [ -f "$file" ]; then
     psql -v ON_ERROR_STOP=1 -U "$DB_ADMIN_USER" -d postgres -f "$file"
